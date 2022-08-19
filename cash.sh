462d8797b5a240179200c04204fb12614157bd89
Opeoluwa <mariafolarin.o@gmail.com>
2022-08-19 05:55:31 +0000

    This is the first commit for our payment gateway

---
+++ cash.sh
@@ -0,0 +1,6 @@
+#!bin/bash/
+#This is the second file related to EFT
+echo "What mode of payment do you prefer: "
+read paymentmode
+echo "Your payment will be done with $paymentmode"
+
---
+++ cheques.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is our running file
+echo "enter your username "
+read username
+echo "Welcome $username to Paymentgateway"
+date
+
+
+
+
